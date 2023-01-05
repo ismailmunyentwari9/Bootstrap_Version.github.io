00290363ae8e0170c81bd4bf94c54833c5749cd4
Ismail <79504482+ismailmunyentwari9@users.noreply.github.com>
2023-01-05 11:52:03 +0200

    Add ES6 (arrow function) to my Js file

--- portfolio.js
+++ portfolio.js
@@ -3,12 +3,12 @@ const close=document.querySelector('.times');
 const menus=document.querySelectorAll('.menu_mobile >li');
 const body=document.querySelector('.blurx');
 const open=document.querySelector('.sp2');
-function closeMenu() {
- menu.style.display="none";
- body.classList.remove('blur');
- open.style.display="flex";
- }
-function openMenu(){
+ const closeMenu = () => {
+  menu.style.display="none";
+  body.classList.remove('blur');
+  open.style.display="flex";
+  }
+  const openMenu = () =>{
   menu.style.display='block';
   open.style.display="none";
   body.classList.add('blur');
